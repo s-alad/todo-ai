79fdab5818e48fa43b1c1aefefd4866cac6b2d50
s-alad <n7.saad@gmail.com>
2023-06-12 23:03:25 -0400

    base ui

--- todo-ai/app/page.tsx
+++ todo-ai/app/page.tsx
@@ -1,9 +1,21 @@
 import Image from 'next/image'
-import s from './page.module.css'
+import s from './page.module.scss'
 
 export default function Home() {
   return (
     <main className={s.main}>
+
+      <div className={s.punchline}>
+        boost your productivity with actionable steps powered by ai.
+      </div>
+      <div className={s.tagline}>
+        no more hassle of brainstorming what needs to be done next; let our AI assistant analyze your tasks, prioritize them, and provide you with clear, personalized action steps. From simple to-do items to complex projects, our AI tool will streamline your workflow and help you achieve your goals faster and more efficiently. 
+      </div>
+
+      <div className={s.todo}>
+
+      </div>
+
     </main>
   )
 }
--- todo-ai/layout/navbar/navbar.tsx
+++ todo-ai/layout/navbar/navbar.tsx
@@ -2,20 +2,25 @@ import React from "react";
 import s from './navbar.module.scss';
 
 export default function Navbar() {
-    return <nav className={s.navbar}>
-        <div className={s.todoai}>todo.ai</div>
+    return <>
+        <nav className={s.navbar}>
+            <div className={s.todoai}>todo.ai</div>
 
-        <div className={s.items}>
-            <div className={s.about}>about</div>
-            <div className={s.team}>help</div>
-        </div>
+            <div className={s.items}>
+                <div className={s.about}>about</div>
+                <div className={s.team}>help</div>
+            </div>
 
-        <div className={s.space}>
+            <div className={s.space}>
 
-        </div>
+            </div>
 
-        <div className={s.actions}>
-            <div className={s.login}>login</div>
-        </div>
-    </nav>;
+            <div className={s.actions}>
+                <div className={s.login}>login</div>
+            </div>
+        </nav>
+
+        <div className={s.seperator}></div>
+
+    </>
 }
