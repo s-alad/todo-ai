67bc007725acc8133133c0a6324440365ce6b72d
s-alad <n7.saad@gmail.com>
2023-06-12 23:35:31 -0400

    more text

--- todo-ai/app/page.tsx
+++ todo-ai/app/page.tsx
@@ -1,15 +1,33 @@
 import Image from 'next/image'
 import s from './page.module.scss'
 
+import "@fortawesome/fontawesome-svg-core/styles.css"; 
+
+import { config } from "@fortawesome/fontawesome-svg-core";
+config.autoAddCss = false; 
+
+import {
+  faBolt,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+
 export default function Home() {
   return (
     <main className={s.main}>
 
       <div className={s.punchline}>
-        boost your productivity with actionable steps powered by ai.
+        boost your productivity with actionable steps
+        <span>
+          <FontAwesomeIcon icon={faBolt} style={{ color: "#2cb67d" }}/>
+        </span> by ai.
       </div>
       <div className={s.tagline}>
-        no more hassle of brainstorming what needs to be done next; let our AI assistant analyze your tasks, prioritize them, and provide you with clear, personalized action steps. From simple to-do items to complex projects, our AI tool will streamline your workflow and help you achieve your goals faster and more efficiently. 
+        no more hassle of brainstorming what needs to be done next; let our AI assistant analyze your tasks, prioritize them, and provide you with clear, personalized action steps. From simple to-do items to complex projects, our AI tool will streamline your workflow and help you achieve your goals faster and efficiently. 
+      </div>
+
+      <div className={s.try}>
+        try it instantly below or <span>sign up</span> to save your tasks. 
       </div>
 
       <div className={s.todo}>
