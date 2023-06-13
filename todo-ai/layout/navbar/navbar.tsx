385ae94677a25c74144ca45b4fe01988f38d126e
s-alad <n7.saad@gmail.com>
2023-06-12 22:47:27 -0400

    basic nav style

---
+++ todo-ai/layout/navbar/navbar.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import s from './navbar.module.scss';
+
+export default function Navbar() {
+    return <nav className={s.navbar}>
+        <div className={s.todoai}>todo.ai</div>
+
+        <div className={s.items}>
+            <div className={s.about}>about</div>
+            <div className={s.team}>help</div>
+        </div>
+
+        <div className={s.space}>
+
+        </div>
+
+        <div className={s.actions}>
+            <div className={s.login}>login</div>
+        </div>
+    </nav>;
+}
