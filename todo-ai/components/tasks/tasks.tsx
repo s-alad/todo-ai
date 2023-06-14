9a7c04c87f8d5a61c885d00d4fd9bd11aaca9177
s-alad <n7.saad@gmail.com>
2023-06-14 18:27:16 -0400

    working instant todo

--- todo-ai/components/tasks/tasks.tsx
+++ todo-ai/components/tasks/tasks.tsx
@@ -5,22 +5,46 @@ import React, { useState } from "react";
 import s from './tasks.module.scss';
 import Task from "../task/task";
 import uid from "@/utils/uid";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAdd, faCirclePlus, faListCheck, faPlus } from "@fortawesome/free-solid-svg-icons";
+import TextareaAutosize from 'react-textarea-autosize';
+
+interface TaskAction {
+    id: string,
+    content: string,
+}
 
 interface TaskInterface {
     id: string,
     content: string,
+    subActions: { [fieldName: string]: TaskAction },
 }
 
 export default function Tasks() {
 
     let [order, setOrder] = useState<Array<string>>([])
-    let [tasks, setTasks] = useState<{[fieldName: string]: TaskInterface}>({})
+    let [tasks, setTasks] = useState<{ [fieldName: string]: TaskInterface }>({})
+
+    let [inputTask, setInputTask] = useState<string>("")
 
     function addEmptyTask() {
-        let newTask: TaskInterface = { id: uid(), content: "" }
+        let newTask: TaskInterface = { id: uid(), content: "", subActions: {}}
         let newTasks = { ...tasks, [newTask.id]: newTask }
         setTasks(newTasks)
         setOrder([...order, newTask.id])
+    }
+
+    function addTask() {
+
+        if (inputTask == "") {
+            return 
+        }
+
+        let newTask: TaskInterface = { id: uid(), content: inputTask, subActions: {}}
+        let newTasks = { ...tasks, [newTask.id]: newTask }
+        setTasks(newTasks)
+        setOrder([...order, newTask.id])
+        setInputTask("")
     }
 
     function changeTaskContent(id: string, content: string) {
@@ -40,19 +64,37 @@ export default function Tasks() {
     }
 
     return (
-        <div className={s.tasks}>
-            {order.map((id: string) => {
-                return (
-                    <Task
-                        key={id}
-                        id={id}
-                        content={tasks[id as keyof Object].content}
-                        contentSetter={changeTaskContent}
-                        taskDeleter={deleteTask}
-                    />
-                )
-            })}
-            <div onClick={addEmptyTask}>add task</div>
+
+        <div className={s.todo}>
+            <div className={s.taskinput}>
+                <FontAwesomeIcon icon={faListCheck} />
+                <TextareaAutosize 
+                    /* type="text" */
+                    placeholder="add task"
+                    value={inputTask}
+                    onChange={(e) => setInputTask(e.target.value)}
+                />
+                <div className={s.add} onClick={addTask}>
+                    <div className={s.circle}>
+                        <FontAwesomeIcon icon={faPlus} />
+                    </div>
+                </div>
+            </div>
+            <div className={s.tasks}>
+                {order.map((id: string) => {
+                    return (
+                        <Task
+                            key={id}
+                            id={id}
+                            content={tasks[id as keyof Object].content}
+                            subActions={tasks[id as keyof Object].subActions}
+                            contentSetter={changeTaskContent}
+                            taskDeleter={deleteTask}
+                        />
+                    )
+                })}
+            </div>
         </div>
+
     )
 }
